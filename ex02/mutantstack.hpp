351bbb0a773f6f3d7a06ef52d424c233c3b94b64
gypark <gghotted@gmail.com>
2021-05-31 00:47:17 +0900

    Update tab to space

--- ex02/mutantstack.hpp
+++ ex02/mutantstack.hpp
@@ -17,18 +17,18 @@ class MutantStack : public std::stack<T>
   };
 
   typedef typename std::stack<T>::container_type::iterator iterator;
-	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+  typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+  typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+  typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
-	iterator begin() { return this->c.begin(); };
-	const_iterator begin() const { return this->c.begin(); };
-	reverse_iterator rbegin() { return this->c.rbegin(); };
-	const_reverse_iterator rbegin() const { return this->c.rbegin(); };
-	iterator end() { return this->c.end(); };
-	const_iterator end() const { return this->c.end(); };
-	reverse_iterator rend() { return this->c.rend(); };
-	const_reverse_iterator rend() const { return this->c.rend(); };
+  iterator begin() { return this->c.begin(); };
+  const_iterator begin() const { return this->c.begin(); };
+  reverse_iterator rbegin() { return this->c.rbegin(); };
+  const_reverse_iterator rbegin() const { return this->c.rbegin(); };
+  iterator end() { return this->c.end(); };
+  const_iterator end() const { return this->c.end(); };
+  reverse_iterator rend() { return this->c.rend(); };
+  const_reverse_iterator rend() const { return this->c.rend(); };
 };
 
 #endif
